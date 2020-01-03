f32b35b389247816f6335018df86dedafd0549f2
Pakkapon Phongthawee <me@pureapp.in.th>
2020-01-03 11:47:41 +0700

    save

--- convert_68kpts.py
+++ convert_68kpts.py
@@ -1,45 +1,37 @@
 import os
-from collections import defaultdict
-from scipy.io import loadmat
 import numpy as np
 from utils.database import COLMAPDatabase,blob_to_array
 
-DATABASE_PATH = 'sqlite/putin_without_projection.db'
-MAT_DIR = 'mat'
-MAT_POSTFIX = '_mesh'
+DATABASE_PATH = 'sqlite/putin_68kpts.db'
+DIRECTORY = 'keypoints'
+POSTFIX = '_kpt.txt'
 IMAGE_EXTENSION = '.jpg'
 if os.path.exists(DATABASE_PATH):
     os.remove(DATABASE_PATH)
 db = COLMAPDatabase.connect(DATABASE_PATH)
 db.create_tables()
 
-mat_files = [f for f in os.listdir('mat') if f[-4:] == '.mat']
+data_files = [f for f in os.listdir(DIRECTORY) if f[-len(POSTFIX):] == POSTFIX]
 
 #add camera
 cam_param = np.asarray([307.2, 128.0, 128.0, 0.0])
-for i in range(len(mat_files)):
+for i in range(len(data_files)):
     cam_id = db.add_camera(model=2, width=256, height=256, params=cam_param)
-    file_name = mat_files[i].split(MAT_POSTFIX)[0] + IMAGE_EXTENSION
+    file_name = data_files[i].split(POSTFIX)[0] + IMAGE_EXTENSION
     db.add_image(file_name,cam_id)
 
 # build keypoint (feature extraction)
 kpt_len = 0
-for image_id, mat_file in enumerate(mat_files,start=1):
-    mesh = loadmat(os.path.join(MAT_DIR,mat_file))
-    kpt_len = mesh['vertices'].shape[0]
-    keypoints = list()
-    #select only highest z to mark at keypoint
-    for i,vertex in enumerate(mesh['vertices'],start=1):
-        x, y, z = vertex
-        keypoints.append( (x,y) )
-    # add keypoints to database
+for image_id, data_file in enumerate(data_files,start=1):
+    keypoints = np.loadtxt(os.path.join(DIRECTORY,data_file))
+    keypoints = keypoints[:,:2]
+    kpt_len = keypoints.shape[0]
     db.add_keypoints(image_id, np.asarray(keypoints))
-
 
 # feature mathcing
 image_matchs = list()
-for from_id in range(1,len(mat_files)+1):
-    for to_id in range(from_id+1,len(mat_files)+1):
+for from_id in range(1,len(data_files)+1):
+    for to_id in range(from_id+1,len(data_files)+1):
         nrange = np.arange(0,kpt_len)
         pairs = np.c_[nrange,nrange]
         # add match to database
--- convert_projection.py
+++ convert_projection.py
@@ -4,7 +4,7 @@ from scipy.io import loadmat
 import numpy as np
 from utils.database import COLMAPDatabase,blob_to_array
 
-DATABASE_PATH = 'sqlite/putin_without_projection.db'
+DATABASE_PATH = 'sqlite/putin_twoview.db'
 MAT_DIR = 'mat'
 MAT_POSTFIX = '_mesh'
 IMAGE_EXTENSION = '.jpg'
@@ -23,28 +23,44 @@ for i in range(len(mat_files)):
     db.add_image(file_name,cam_id)
 
 # build keypoint (feature extraction)
-kpt_len = 0
+images_keypoints = dict()
+images_keypoints_map = defaultdict(dict)
 for image_id, mat_file in enumerate(mat_files,start=1):
     mesh = loadmat(os.path.join(MAT_DIR,mat_file))
-    kpt_len = mesh['vertices'].shape[0]
-    keypoints = list()
+    image_projector = defaultdict(dict)
     #select only highest z to mark at keypoint
     for i,vertex in enumerate(mesh['vertices'],start=1):
-        x, y, z = vertex
-        keypoints.append( (x,y) )
+        x, y, z = np.around(vertex).astype(np.int32)
+        if z > 0 and (not y in image_projector[x] or z > image_projector[x][y]['z']):
+            image_projector[x][y] = {
+                'z': z,
+                'v': i #vertex number
+            }
+    images_keypoints[image_id] = list()
+    len_kpt = 0
+    for x in image_projector.keys():
+        for y in image_projector[x].keys():
+            kpt = image_projector[x][y]
+            images_keypoints[image_id].append( (x,y) )
+            images_keypoints_map[image_id][kpt['v']] = len_kpt
+            len_kpt = len_kpt + 1
     # add keypoints to database
-    db.add_keypoints(image_id, np.asarray(keypoints))
+    db.add_keypoints(image_id, np.asarray(images_keypoints[image_id]))
 
 
 # feature mathcing
 image_matchs = list()
 for from_id in range(1,len(mat_files)+1):
     for to_id in range(from_id+1,len(mat_files)+1):
-        nrange = np.arange(0,kpt_len)
-        pairs = np.c_[nrange,nrange]
+        pairs = list()
+        for kpt_index,kpt_marker in enumerate(images_keypoints_map[from_id]):
+            if kpt_marker in images_keypoints_map[to_id]:
+                pairs.append(
+                    (kpt_index, images_keypoints_map[to_id][kpt_marker])
+                )
         # add match to database
         #db.add_matches(from_id, to_id, np.asarray(pairs))
-        db.add_two_view_geometry(from_id, to_id, pairs)
+        db.add_two_view_geometry(from_id, to_id, np.asarray(pairs))
 
 # commit (save) to db 
 db.commit()
